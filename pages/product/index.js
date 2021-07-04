3a7469629abd2c6c76acf03d49b31445398165b5
MdTasib <ohidulalam442@gmail.com>
2021-07-04 15:21:24 +0600

    nested routing

--- pages/product/index.js
+++ pages/product/index.js
@@ -1,11 +1,4 @@
 function Product() {
-    return (
-        <>
-            <h2>Product 1</h2>
-            <h2>Product 2</h2>
-            <h2>Product 3</h2>
-        </>
-    )
+    return <h2>this is product page</h2>
 }
-
 export default Product;
