8796bf12ea89c100568b98d17fd42ae093cbdeba
MdTasib <ohidulalam442@gmail.com>
2021-07-12 11:10:18 +0600

    getStaticProps & getStaticPaths

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,16 @@
+import Link from 'next/link';
 function Home() {
   return (
     <div>
       <h2>NEXT JS pre-rendering</h2>
+
+      <Link href='/users' passHref>
+        <button>Users</button>
+      </Link>
+
+      <Link href='/posts' passHref>
+        <button>Posts</button>
+      </Link>
     </div>
   )
 }
